6bf55b8dfc53975cc6dad38bd349d1a47c89bcc7
Chr1stian <christiannyvoll@gmail.com>
2023-05-09 15:55:09 +0200

    Fix linting

--- src/scripts/create-and-tag-docker-image.sh
+++ src/scripts/create-and-tag-docker-image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# shellcheck disable=SC2086
 if [ ${TAG_LATEST} ]; then
   echo "creating image $DOCKER_REGISTRY/$CIRCLE_PROJECT_REPONAME:$TAG with tag 'latest'"
+  # shellcheck disable=SC2094
   docker build -t "$DOCKER_REGISTRY"/"$CIRCLE_PROJECT_REPONAME":"$TAG" >> -t latest .
 else
   echo "creating image $DOCKER_REGISTRY/$CIRCLE_PROJECT_REPONAME:$TAG without tag 'latest'"
