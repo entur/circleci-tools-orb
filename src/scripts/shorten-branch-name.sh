99d90b1e038f272f7435e82a79f8c1bccd3cd5f4
Chr1stian <christiannyvoll@gmail.com>
2023-05-11 10:13:45 +0200

    Remove test line

--- src/scripts/shorten-branch-name.sh
+++ src/scripts/shorten-branch-name.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export BRANCH_NAME="long-test-branc-æ.å.øÆÆ-name.with_some_spec/ial^characters.to-be-truncated"
 # Shorten to max length
 # shellcheck disable=SC2001
 SHORT_BRANCH_NAME=${BRANCH_NAME:0:55}
