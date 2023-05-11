e4ecc36c36e0b0a7fa8f122af7b4f5acb83d9607
Chr1stian <christiannyvoll@gmail.com>
2023-05-11 10:00:54 +0200

    Fix linting errors

--- src/scripts/shorten-branch-name.sh
+++ src/scripts/shorten-branch-name.sh
@@ -2,7 +2,7 @@
 
 # Shorten to max length and removes any "-" if last:
 # shellcheck disable=SC2001
-SHORT_BRANCH_NAME=$(echo ${$BRANCH_NAME:0:55} | sed s'/[-]$//')
+SHORT_BRANCH_NAME=$(echo ${"$BRANCH_NAME":0:55} | sed s'/[-]$//')
 # Converts to lowercase and removes Norwegian characters:
 # shellcheck disable=SC2021
 SHORT_BRANCH_NAME=$(echo "$SHORT_BRANCH_NAME" | tr '[:upper:]' '[:lower:]' | tr -d '[ÆØÅæøå]')
