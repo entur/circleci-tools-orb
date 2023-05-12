a148f2c54547a7c6073ca8465111ca996abbaf11
Chr1stian <christiannyvoll@gmail.com>
2023-05-12 20:18:00 +0200

    Use BASH_ENV SHORT_BRANCH_NAME or BRANCH_NAME if not set

--- src/scripts/create-tag.sh
+++ src/scripts/create-tag.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Branch to create tag for: $BRANCH_NAME, shortened version: $SHORT_BRANCH_NAME"
+if [ "$SHORT_BRANCH_NAME" == null ]; then
+  SHORT_BRANCH_NAME=$BRANCH_NAME
+fi
 # shellcheck disable=SC2021
-TAG=$(echo "$BRANCH_NAME"."$(date '+%Y%m%d')"-"${CIRCLE_SHA1:0:7}"-"${CIRCLE_WORKFLOW_ID:0:4}" | tr '[:upper:]/_' '[:lower:]--' | tr -d '[ÆØÅæøå]')
+TAG=$(echo "$SHORT_BRANCH_NAME"."$(date '+%Y%m%d')"-"${CIRCLE_SHA1:0:7}"-"${CIRCLE_WORKFLOW_ID:0:4}" | tr '[:upper:]/_' '[:lower:]--' | tr -d '[ÆØÅæøå]')
 echo "export $TAG" >> "$BASH_ENV"
 echo "TAG created: $TAG"
