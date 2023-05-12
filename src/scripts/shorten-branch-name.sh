612f0bf0f3157eb92aaa2d6e872666929d87768a
Chr1stian <christiannyvoll@gmail.com>
2023-05-12 14:27:33 +0200

    Shellcheck

--- src/scripts/shorten-branch-name.sh
+++ src/scripts/shorten-branch-name.sh
@@ -5,7 +5,7 @@ echo "Branch name to shorten: $BRANCH_NAME"
 # shellcheck disable=SC2001
 SHORT_BRANCH_NAME=${BRANCH_NAME:0:55}
 # and removes any "-" if last:
-# shellcheck disable=SC2021
+# shellcheck disable=SC2021,SC2001
 SHORT_BRANCH_NAME=$(echo "$SHORT_BRANCH_NAME" | sed s'/[-]$//')
 # Converts to lowercase and removes Norwegian characters:
 # shellcheck disable=SC2021
