78664c14f579f14191170b6de7e638e91bf114d8
Chr1stian <christiannyvoll@gmail.com>
2023-05-12 14:09:31 +0200

    Fix commands

--- src/scripts/create-tag.sh
+++ src/scripts/create-tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # shellcheck disable=SC2021
-echo "export TAG=$(echo "$BRANCH_NAME"."$(date '+%Y%m%d')"-"${CIRCLE_SHA1:0:7}"-"${CIRCLE_WORKFLOW_ID:0:4}" | tr '[:upper:]/_' '[:lower:]--' | tr -d '[ÆØÅæøå]')" >> "$BASH_ENV"
+TAG=$(echo "$BRANCH_NAME"."$(date '+%Y%m%d')"-"${CIRCLE_SHA1:0:7}"-"${CIRCLE_WORKFLOW_ID:0:4}" | tr '[:upper:]/_' '[:lower:]--' | tr -d '[ÆØÅæøå]')
+echo "export $TAG" >> "$BASH_ENV"
 echo "TAG created: $TAG"
